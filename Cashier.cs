a802dc5a8553f2fc02224b58f18d6a9fa54b3900
Angel <angelsanchezm1856@gmail.com>
2025-11-25 01:05:48 -0500

    Filled out Cashier class structure

--- Cashier.cs
+++ Cashier.cs
@@ -1,6 +1,14 @@
 namespace Final_Project;
 
-public class Cashier
+public class Cashier : Employee
 {
-    
+    public decimal HourlyPay { get; set; }
+
+    public Cashier(string name, int id, decimal hourlyPay)
+        : base(name, id)
+    {
+        HourlyPay = hourlyPay;
+    }
+    public void GetJobDescription(){}
+    public void GetTaskList(){}
 }
