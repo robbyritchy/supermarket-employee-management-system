51c8a17dd5a1f2643206522a44d19f8a4efc3142
Angel <angelsanchezm1856@gmail.com>
2025-11-25 00:56:07 -0500

    Filled in methods for Manager class, and added placeholders

--- Manager.cs
+++ Manager.cs
@@ -6,7 +6,19 @@ public class Manager : IManageEmployees, IWork
     public int ID { get; set; }
     public decimal Salary { get; set; }
 
-    GetJobDescription();
-    GetTaskList();
+    //IWork methods
+    public void GetJobDescription(){}
+    public void GetTaskList(){}
+    
+    //IManageEmployees methods
+    public void HireEmployee(Employee employee){}
+    public void FireEmployee(int employeeId){}
+    public void PayEmployee(int employeeId){}
+    public void EditEmployee(int employeeId, string newName, decimal? newSalaryOrHourly = null){}
 
+    //*** Placeholder value for now, make sure to fix later ***
+    public decimal GetEmployeePay(int employeeId, IPaycheck method)
+    {
+        return 0;
+    }
 }
