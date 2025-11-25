8d956860a8a1e6350a98f71d0f5982fb1e996e8b
Angel <angelsanchezm1856@gmail.com>
2025-11-25 01:06:29 -0500

    Filled out Stocker class structure

--- Stocker.cs
+++ Stocker.cs
@@ -1,6 +1,16 @@
 namespace Final_Project;
 
-public class Stocker
+namespace Final_Project;
+
+public class Stocker : Employee
 {
-    
+    public decimal HourlyPay { get; set; }
+
+    public Stocker(string name, int id, decimal hourlyPay)
+        : base(name, id)
+    {
+        HourlyPay = hourlyPay;
+    }
+    public void GetJobDescription(){}
+    public void GetTaskList(){}
 }
