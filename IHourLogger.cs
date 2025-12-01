f41e358b62068eb39e04bf8798eebc6767ee319c
Angel <angelsanchezm1856@gmail.com>
2025-12-01 13:16:27 -0500

    Added an HourLogger interface for easier access

---
+++ IHourLogger.cs
@@ -0,0 +1,8 @@
+﻿namespace Final_Project;
+
+public interface IHourLogger
+{
+    void AddHours(int employeeId, WorkLogEntry entry);
+    IEnumerable<WorkLogEntry> GetHours(int employeeId);
+    double GetTotalHours(int employeeId);
+}
