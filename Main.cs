7c89fd497e01e354ecd69696d0e7c28c956d5d40
Angel <angelsanchezm1856@gmail.com>
2025-12-01 02:51:18 -0500

    Started working on prototype of main console ui

--- Main.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Final_Project;
-
-public class Main
-{
-    
-}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var supermarket = new Supermarket();
+        var owner = new Manager("Owner", supermarket.GenerateId(), 5000, supermarket.Employees);
+
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("\n---------- Supermarket Management ----------");
+            Console.WriteLine("1. Show Employees");
+            Console.WriteLine("2. Hire Employee");
+            Console.WriteLine("3. Edit Employee");
+            Console.WriteLine("4. Fire employee");
+            Console.WriteLine("5. Exit");
+            Console.Write("\nChoose an option: ");
+            
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                
+            }
+        }
     }
 }
